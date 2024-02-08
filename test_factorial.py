2c27c12f8bb589310f0b13c06ab287fb5c68b0f4
rhamdi88 <hamdirami88@gmail.com>
2024-02-08 10:18:13 +0300

    new change test

--- test_factorial.py
+++ test_factorial.py
@@ -4,7 +4,7 @@ from factorial import factorial
 class TestFactorial(unittest.TestCase):
     def test_factorial(self):
         self.assertEqual(factorial(0), 1)
-        self.assertEqual(factorial(1), 1)
+        self.assertEqual(factorial(1), 6)
         self.assertEqual(factorial(5), 120)
     
     def test_factorial_negative(self):
